7efeb8cdbbe8d81572d92e3f4d9c3da9a99c6dd2
JeniLopez <91500161+JeniLopez@users.noreply.github.com>
2022-03-16 11:31:11 -0300

    segunda version del codigo

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');  
+var app = express();  
+var bodyParser = require('body-parser');   
+var urlencodedParser = bodyParser.urlencoded({ extended: false })  
+app.use(express.static('public'));  
+app.get('/index.html', function (req, res) {  
+   res.sendFile( __dirname  + "index.html" );  
+})  
+app.post('/process_post', urlencodedParser, function (req, res) {  
+   response = {Bienvenido:req.body.first_name,j:req.body.second_name}
+   console.log(response);  
+   res.end(JSON.stringify(response));  
+})  
+var server = app.listen(8000, function () {  
+  var host = server.address().address  
+  var port = server.address().port  
+  console.log("Example app listening at http://%s:%s", host, port)  
+})
